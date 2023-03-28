c89af79e9f67c61240cd03605af777cfd43746d7
Olga F <krasota1_00@mail.ru>
2023-03-28 20:01:07 +0700

    modification

--- src/test/java/ru/netology/domain/BillboardManagerTest.java
+++ src/test/java/ru/netology/domain/BillboardManagerTest.java
@@ -43,7 +43,7 @@ public class BillboardManagerTest {
 
     @Test
     public void returnTheEntireArray() {
-        BillboardManager manager = new BillboardManager(11);
+        BillboardManager manager = new BillboardManager();
         manager.addFilm("Film 1");
         manager.addFilm("Film 2");
         manager.addFilm("Film 3");
@@ -64,7 +64,7 @@ public class BillboardManagerTest {
 
     @Test
     public void FindOneMovie() {
-        BillboardManager manager = new BillboardManager();
+        BillboardManager manager = new BillboardManager(10);
         manager.addFilm("Film 1");
 
         String[] expected = {"Film 1"};
@@ -75,7 +75,7 @@ public class BillboardManagerTest {
 
     @Test
     public void oneMovieTheArray() {
-        BillboardManager manager = new BillboardManager();
+        BillboardManager manager = new BillboardManager(10);
         manager.addFilm("Film 8");
         manager.addFilm("Film 9");
         manager.addFilm("Film 10");
@@ -89,7 +89,7 @@ public class BillboardManagerTest {
 
     @Test
     public void BillboardManager1() {
-        BillboardManager manager = new BillboardManager();
+        BillboardManager manager = new BillboardManager(10);
         manager.addFilm("Film 10");
 
         String[] expected = {"Film 10"};
@@ -112,7 +112,7 @@ public class BillboardManagerTest {
 
     @Test
     public void findLast() {
-        BillboardManager manager = new BillboardManager();
+        BillboardManager manager = new BillboardManager(10);
         manager.addFilm("Film 1");
         manager.addFilm("Film 2");
         manager.addFilm("Film 3");
@@ -127,7 +127,7 @@ public class BillboardManagerTest {
 
     @Test
     public void findLast1() {
-        BillboardManager manager = new BillboardManager();
+        BillboardManager manager = new BillboardManager(10);
         manager.addFilm("Film 1");
         manager.addFilm("Film 2");
         manager.addFilm("Film 3");
@@ -147,7 +147,7 @@ public class BillboardManagerTest {
 
     @Test
     public void findLast2() {
-        BillboardManager manager = new BillboardManager(11);
+        BillboardManager manager = new BillboardManager(10);
         manager.addFilm("Film 1");
         manager.addFilm("Film 2");
         manager.addFilm("Film 3");
@@ -160,7 +160,7 @@ public class BillboardManagerTest {
         manager.addFilm("Film 10");
         manager.addFilm("Film 11");
 
-        String[] expected = {"Film 11", "Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] expected = {"Film 11", "Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
